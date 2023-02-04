40ac641cf787da12e3b7210534a5b2581fa62c5f
NishaBisht493 <81354503+NishaBisht493@users.noreply.github.com>
2023-02-05 01:30:49 +0530

    Add files via upload

---
+++ 23. LCS.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int lcs(string &first, string &second){
+    vector<vector<int>> dp(first.size()+1,vector<int>(second.size()+1,0));
+
+    for(int i = 1 ; i <= first.size() ; i++){
+        for(int j = 1 ; j <= second.size() ; j++){
+            if(first[i-1] == second[j-1]){
+                dp[i][j] = 1+dp[i-1][j-1];
+            }else{
+                dp[i][j] = max(
+                    dp[i-1][j-1],
+                    max(
+                        dp[i-1][j],
+                        dp[i][j-1]
+                    )
+                );
+            }
+        }
+    }
+
+    return dp.back().back();
+}
+
+int main(){
+    
+}
